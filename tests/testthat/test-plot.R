dfbdf9449e21bb2ce3a46f19b5313eba15fcf589
Yorick <yongzhuo.chen@yale.edu>
2022-11-08 13:19:39 -0500

    Initial commit.

---
+++ tests/testthat/test-plot.R
@@ -0,0 +1,34 @@
+test_that(
+  "The accel_plot() returns a ggplot object.",
+  {
+    data(ukb_accel)
+    p =  accel_plot(ukb_accel[1:100,])
+    expect_true(inherits(p, "gg"))
+  }
+)
+
+test_that(
+  "The accel_plot() errors when no time or freq column.",
+  {
+    data(iris)
+    expect_error(accel_plot(iris))
+  }
+)
+
+test_that(
+  "The accel_plot() is correct for time-series data.",
+  {
+    data(ukb_accel)
+    p = accel_plot(ukb_accel[1:100,]) + ggplot2::theme_minimal()
+    expect_doppelganger("first-100-samples", p)
+    #you can review graphically with testthat::snapshot_review().
+  }
+)
+
+test_that(
+  "The accel_plot() errors when no time or freq column.",
+  {
+    data(iris)
+    expect_error(accel_plot(iris))
+  }
+)
