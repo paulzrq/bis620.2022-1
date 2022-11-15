cdef8691d5859fdb709a5627505180c3fdbeb9e4
Yorick <yongzhuo.chen@yale.edu>
2022-11-15 17:46:22 -0500

    commit

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -44,3 +44,13 @@ test_that(
     expect_true(inherits(p, "gg"))
   }
 )
+test_that(
+  "The accel_plot() returns a ggplot object.",
+  {
+    data(ukb_accel)
+    p <-  spectral_signature(ukb_accel[1:100, ],
+                             take_log = TRUE)
+
+    expect_true(inherits(p, "data.frame"))
+  }
+)
